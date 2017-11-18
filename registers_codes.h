7256281f003f71ed70c3b676454bf263bb15eaf1
Ubuntu <ubuntu@ip-172-31-5-76.us-west-2.compute.internal>
2017-11-18 12:05:04 -0500

    initial upload

---
+++ registers_codes.h
@@ -0,0 +1,31 @@
+#ifndef REGISTERS_CODES_H_
+#define REGISTERS_CODES_H_
+
+// Registers
+int rax;
+int rcx;
+int rdx;
+int rbx;
+int rsp;
+int rbp;
+int rsi;
+int rdi;
+int r8;
+int r9;
+int r10;
+int r11;
+int r12;
+int r13;
+int r14;
+
+
+int of; // Overflow flag
+int sf; // Sign flag
+int zf; // Zero flag
+
+int PC; // Program Counter
+
+int memory[512];
+
+
+#endif
